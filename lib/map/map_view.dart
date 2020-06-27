26b5f364a2716717bbc57b73fcaccd7bdb59bb1a
Prince Nna <stormprince77@gmail.com>
2020-06-27 16:08:47 +0100

    🐛 FIX: Remove map when not in view.

--- lib/map/map_view.dart
+++ lib/map/map_view.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:logger/logger.dart';
 
-class MapView extends StatelessWidget {
+import '../core/app/locator.dart';
+import '../core/services/bottom_nav_bar/bottom_nav_bar_service.dart';
+
+class MapView extends StatefulWidget {
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  final BottomNavBarService _bottomNavBarService =
+      locator<BottomNavBarService>();
+
   @override
   Widget build(BuildContext context) {
-    print('Building Map');
-    return GoogleMap(
+    Logger().i('Building Map');
+    final googleMap = GoogleMap(
       initialCameraPosition: CameraPosition(
         target: LatLng(
-          /*model.currentUserLocation?.latitude ??*/ 0.0,
-          /*model.currentUserLocation?.longitude ??*/ 0.0,
+          0.0,
+          0.0,
         ),
       ),
+      onMapCreated: (ctr) {
+        print('Created');
+      },
       myLocationEnabled: true,
       myLocationButtonEnabled: false,
       zoomControlsEnabled: false,
+    );
+
+    return Obx(
+      () => _bottomNavBarService.index == 0 ? googleMap : Container(),
     );
   }
 }
